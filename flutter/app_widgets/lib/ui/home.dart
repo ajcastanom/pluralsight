b342081e26d6ce4cbd619d7cedf4805d9653efe3
Anderson <ajcastan@bancolombia.com.co>
2019-02-28 08:52:46 -0500

    Se agregan filas y columnas.

--- flutter/app_widgets/lib/ui/home.dart
+++ flutter/app_widgets/lib/ui/home.dart
@@ -3,19 +3,71 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(
+    return Center(
+        child: Container(
       alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 30.0, left: 10.0),
       color: Colors.deepOrangeAccent,
-      child: Text("Pizza",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 80.0,
-            decoration: TextDecoration.none,
-            fontFamily: "Oxygen",
-            fontWeight: FontWeight.normal,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  "Marinara",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: "Oxygen",
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "Tomato, Garlic",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: "Oxygen",
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
           ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  "Margherita",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: "Oxygen",
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  "Tomato, Mozzarella, Basil",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: "Oxygen",
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     ));
   }
-
 }
