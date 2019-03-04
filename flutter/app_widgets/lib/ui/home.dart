48ed83ae9f013266a8ce4bb7cab35d790f434304
Anderson <ajcastan@bancolombia.com.co>
2019-03-04 13:26:30 -0500

    Se agrea boton

--- flutter/app_widgets/lib/ui/home.dart
+++ flutter/app_widgets/lib/ui/home.dart
@@ -67,6 +67,7 @@ class Home extends StatelessWidget {
             ],
           ),
           PizzaImageWidget(),
+          OrderButton(),
         ],
       ),
     ));
@@ -86,4 +87,36 @@ class PizzaImageWidget extends StatelessWidget {
       child: image,
     );
   }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+          child: Text("Order your pizza!"),
+          color: Colors.lightGreen,
+          elevation: 5.0,
+          onPressed: () {
+            order(context);
+          },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for you order"),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => alert,
+    );
+
+  }
+
 }
