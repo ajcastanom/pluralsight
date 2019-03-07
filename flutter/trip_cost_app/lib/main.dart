1f47f586b9e5538f322db2a7e1a68952bf42f09f
Anderson <ajcastan@bancolombia.com.co>
2019-03-07 14:42:31 -0500

    Se completa app trip cost

--- flutter/trip_cost_app/lib/main.dart
+++ flutter/trip_cost_app/lib/main.dart
@@ -7,28 +7,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello you',
+      title: 'Trip Cost Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new HelloYou(),
+      home: new FuelForm(),
     );
   }
 }
 
-class HelloYou extends StatefulWidget {
+class FuelForm extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _HelloYouState();
-
+  State<StatefulWidget> createState() => _FuelFormState();
 }
 
-class _HelloYouState extends State<HelloYou> {
-  String name = '';
+class _FuelFormState extends State<FuelForm> {
+  String result = '';
   final _currencies = ['Dollars', 'Euro', 'Pounds'];
+  final double _formDistance = 5.0;
   String _currency = 'Dollars';
+  TextEditingController distanceController = TextEditingController();
+  TextEditingController avgController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.title;
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello"),
@@ -38,29 +42,100 @@ class _HelloYouState extends State<HelloYou> {
         padding: EdgeInsets.all(15.0),
         child: Column(
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Please insert your name"
-              ),
-              onChanged: (String string) {
+            Padding(
+                padding: EdgeInsets.only(
+                  top: _formDistance,
+                  bottom: _formDistance,
+                ),
+                child: TextField(
+                  controller: distanceController,
+                  decoration: InputDecoration(
+                    labelText: 'Distance',
+                    hintText: "e.g. 124",
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                  top: _formDistance,
+                  bottom: _formDistance,
+                ),
+                child: TextField(
+                  controller: avgController,
+                  decoration: InputDecoration(
+                    labelText: 'Distance per Unit',
+                    hintText: "e.g. 17",
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                  top: _formDistance,
+                  bottom: _formDistance,
+                ),
+                child: Row(children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                    controller: priceController,
+                    decoration: InputDecoration(
+                      labelText: 'Price',
+                      hintText: "e.g. 1.65",
+                      labelStyle: textStyle,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  )),
+                  Container(width: _formDistance * 5,),
+                  Expanded(
+                      child: DropdownButton<String>(
+                    items: _currencies.map((String value) {
+                      return DropdownMenuItem<String>(
+                          value: value, child: Text(value));
+                    }).toList(),
+                    value: _currency,
+                    onChanged: (String value) {
+                      _onDropdownChanged(value);
+                    },
+                  ))
+                ])),
+            Row(children: <Widget>[
+              Expanded(child: RaisedButton(
+              color: Theme.of(context).primaryColorDark,
+              textColor: Theme.of(context).primaryColorLight,
+              onPressed: () {
                 setState(() {
-                  name = string;
+                  result = _calculate();
                 });
               },
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value)
-                );
-              }).toList(),
-              value: _currency,
-              onChanged: (String value) {
-                _onDropdownChanged(value);
+              child: Text(
+                'Submit',
+                textScaleFactor: 1.5,
+              ),
+            )),
+            Expanded(child: RaisedButton(
+              color: Theme.of(context).buttonColor,
+              textColor: Theme.of(context).primaryColorDark,
+              onPressed: () {
+                setState(() {
+                  _reset();
+                });
               },
-            ),
-            Text("Hello " + name + "!")
+              child: Text(
+                'Reset',
+                textScaleFactor: 1.5,
+              ),
+            ))]),
+            Text(result)
           ],
         ),
       ),
@@ -70,6 +145,27 @@ class _HelloYouState extends State<HelloYou> {
   _onDropdownChanged(String value) {
     setState(() {
       this._currency = value;
+    });
+  }
+
+  String _calculate() {
+    double _distance = double.parse(distanceController.text);
+    double _fuelCost = double.parse(priceController.text);
+    double _consumption = double.parse(avgController.text);
+    double _totalCost = _distance / _consumption * _fuelCost;
+    String _result = 'The total cost of your trip is ' +
+        _totalCost.toStringAsFixed(2) +
+        ' ' +
+        _currency;
+    return _result;
+  }
+
+  void _reset() {
+    distanceController.text = '';
+    priceController.text = '';
+    avgController.text = '';
+    setState(() {
+      result = '';
     });
   }
 }
